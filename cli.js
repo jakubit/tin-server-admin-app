347b98c846f1bd4367e9d6fdfe5eab495a844414
jakub <pawlak011@gmail.com>
2019-01-02 00:14:55 +0100

    Main menu handler added

--- cli.js
+++ cli.js
@@ -4,6 +4,7 @@ const figlet = require('figlet');
 const CLI = require('clui');
 const Spinner = CLI.Spinner;
 const status = new Spinner('Authenticating you, please wait...');
+const features = require('./lib/features');
 
 clear();
 console.log(
@@ -14,6 +15,52 @@ console.log(
 
 const inquirer = require('./lib/inquirer');
 
+const menuHandler = () => {
+  inquirer.showMenu().then((answer) => {
+    console.log(answer);
+    switch (answer.option) {
+      case '1':
+        // Create new user
+        features.createNewUser();
+        break;
+      case '2':
+        // Delete user
+        features.deleteUser();
+        break;
+      case '3':
+        // Edit user
+        features.editUser();
+        break;
+      case '4':
+        // Show users
+        features.showUsers();
+        break;
+      case '5':
+        // Show catalog
+        features.showCatalog();
+        break;
+      case '6':
+        // Delete file
+        features.deleteFile();
+        break;
+      case '7':
+        // Turn on the service
+        features.turnOnService();
+        break;
+      case '8':
+        // Turn off the service
+        features.turnOffService();
+        break;
+      case '9':
+        break;
+    };
+    return answer.option;
+  }).then((option) => {
+    if (option != 9)
+      menuHandler();
+  });
+}
+
 const run = async () => {
     const credentials = await inquirer.askAdminCredentials();
     /*
@@ -23,14 +70,13 @@ const run = async () => {
     * 3. Jesli ok to wyswietl menu, jesli nie ok to wyswietl blad
     */
     status.start();
+
     setTimeout(() => {
       status.stop();
       console.log(credentials);
 
       // Pokaz menu
-      inquirer.showMenu().then((answer) => {
-        console.log(answer);
-      });
+      menuHandler();
 
     }, 1000);
 
